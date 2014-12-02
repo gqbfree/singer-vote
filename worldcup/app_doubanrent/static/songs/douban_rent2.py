10397b34bd48b7ee16fc5cf81472d35d4f3f940c
martin <gqbfree@msn.com>
2014-12-02 19:03:44 +0800

    little modification

---
+++ worldcup/app_doubanrent/static/songs/douban_rent2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+from scrapy.spider import Spider 
+from scrapy.selector import Selector
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+
+from douban_rent.items import DoubanRentItem
+
+
+class doubanRentSpider(Spider):
+    name = "douban_rent"
+    start_urls = ["http://www.douban.com/group/shanghaizufang/"]
+    rules = [
+            Rule(SgmlLinkExtractor(allow=(r'http://www.douban.com/group/shanghaizufang/discussion\?start=\d+'))),
+            Rule(SgmlLinkExtractor(allow=(r'http://www.douban.com/group/topic/\d+', callback="parse"))),
+    ]
+
+    def parse(self, response):
+        sel  = Selector(response)
+        item = DoubanRentItem() 
+
+        item['title'] = sel.xpath('//*[@id="content"]/h1').extract()
+        item['link']  = response.url
+        item['desc']  = sel.xpath('//*[@id="link-report"]/div').extract()
+		
+		return item
+
