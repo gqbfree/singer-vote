ca6e99e960b46cd59604ab93c509d6b45c571f6c
martin <gqbfree@msn.com>
2014-12-02 20:18:56 +0800

    page adding

--- worldcup/app_doubanrent/views/doubanrent.py
+++ worldcup/app_doubanrent/views/doubanrent.py
@@ -4,34 +4,53 @@ from django.http import HttpResponse, HttpResponseRedirect
 import os,re,datetime
 import json
 
-def doubanrent_read(pagenum):
+def doubanrent_read(page_num):
     rent_info = []
-    total     = pagenum * 10
+    list_dict = {}
+    total_num = 100
+    total_page= 0
+    start     = 0
+    item_count= 0
+#    with open("/home/qingbo_gao/martin/scrapy/project/douban_rent/douban_info_utf8.json") as f:
+#        j = json.loads(f.readline())
+#        if not j:
+#            total_num = int("".join(j['total_num']))
+#    print total_num
+
+    total_page = total_num/10 + 1
+
+    if page_num == 0 or total_page == 1:
+        page_num = 1
+    elif page_num > total_page:
+        page_num = total_page
+
+    start = (page_num - 1) * 10 + 1
     with open("/home/qingbo_gao/martin/scrapy/project/douban_rent/douban_data_utf8.json") as f:
         for line in f.readlines():
-            j = json.loads(line)   
-            rent_info.append([j['title'], j['link'], j['desc']])
-            total -= 1
-            if not total:
+            j = json.loads(line) 
+            item_count += 1  
+            if item_count < start:
+                continue
+            elif item_count > start + 10 :
                 break
-    
-    return rent_info
+            else:
+                rent_info.append([j['title'], j['link'], j['desc']])
+
+    list_dict = {'rent_info':rent_info, 'response_msg':'', 'page_num':page_num, 'page_prev':page_num-1, 'page_next':page_num+1} 
+    return list_dict 
 
 
 def doubanrent_redirect(response_msg, rent_info, page_num):
-    rent_info = doubanrent_read(page_num)
-    list_dict = {'rent_info':rent_info, 'response_msg':response_msg} 
+    list_dict = doubanrent_read(page_num)    
     return render_to_response('doubanrent_display.html', list_dict)
 
 
-def doubanrent_display(request):
-    page     = request.POST.get("page", '')
-    district = request.POST.get("district", '')
-
+def doubanrent_display(request, page):
     response_msg = ''
     rent_info    = []
     page_num     = 0 
 
+    district = request.POST.get("district", '')
     if not page.isdigit():
         return doubanrent_redirect(response_msg, rent_info, 1)
     else:
